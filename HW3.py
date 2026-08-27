2d31bcb3e89a8b2b96d8249701b41662998a7a35
Jacob Gwyn <jg126000@student.wpsok.org>
2026-08-27 13:08:26 -0500

    Submitted HW3.

---
+++ HW3.py
@@ -0,0 +1,32 @@
+#Name: Jacob Gwyn
+#Class: 5th Hour
+#Assignment: HW3
+
+
+#1. Print "Hello World!"
+print("hello world")
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+NumTen = 10
+
+Words = "My_World"
+
+Box = False
+
+#3. Print all three variables on the same print function (at the same time).
+print(NumTen, Words, Box)
+#4. Create a variable that asks the user to input an integer.
+X = int(input("give me a number"))
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+int_sum = NumTen + X
+print(int_sum)
+#6. Take the result from #5 and divide it by 2. Print the result.
+print(int_sum / 2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+Box = True
+#8. Print the value of the boolean variable.
+print(Box)
+#9. Create a variable with a number that contains decimals.
+Decimal_Ex = 1.5
+#10. Round the number from #9 up or down using the round function.
+print(round(Decimal_Ex))
+
